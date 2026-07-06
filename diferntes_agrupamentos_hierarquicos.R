fe7ea4cc024bc566b3ca89e8d49674626645c389
Edbbioeco <edsonbbiologia@gmail.com>
2026-07-05 21:24:15 -0300

    Gerar múltiplos agrupamentos por função implícita dentro do purrr::map2()

--- diferntes_agrupamentos_hierarquicos.R
+++ diferntes_agrupamentos_hierarquicos.R
@@ -79,45 +79,35 @@ agrup_hie
 
 ### Gráfico ----
 
-gg_dendrodata <- function(agrup_hie, metodos){
-
-  dendro_plot <- ggplot() +
-    geom_segment(data = agrup_hie$segments, aes(x = x, y = y,
-                                                xend = xend, yend = yend),
-                 linewidth = 1) +
-    labs(x = NULL,
-         y = "Euclidean Distance",
-         title = paste0("Método = ", metodos)) +
-    scale_x_continuous(breaks = 1:11,
-                       labels = agrup_hie$labels$label) +
-    scale_y_continuous(expand = FALSE) +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1,
-                                     color = "black", size = 25),
-          axis.text = element_text(color = "black", size = 25),
-          axis.title = element_text(color = "black", size = 25),
-          title = element_text(color = "black", size = 25),
-          panel.grid.major.y = element_line(linewidth = 1, color = "gray80"),
-          panel.grid.minor.y = element_blank(),
-          panel.grid.major.x = element_blank(),
-          panel.grid.minor.x = element_blank()) +
-    ggview::canvas(height = 10, width = 12)
-
-  print(dendro_plot)
-
-  ggsave(filename = paste0("agrupamento_hierarquico_",
-                           metodos,
-                           ".png"),
-         height = 10, width = 12)
-
-
-}
-
-agrup_hie <- ls(pattern = "dendrodata_") |>
-  mget(envir = globalenv())
-
-agrup_hie
-
-purrr::walk2(agrup_hie,
+purrr::map2(agrup_hie,
              metodos,
-             gg_dendrodata)
+             purrr::in_parallel(
+
+               ~ggplot() +
+                 geom_segment(data = .x$segments,
+                              aes(x = x,
+                                  y = y,
+                                  xend = xend,
+                                  yend = yend),
+                              linewidth = 1) +
+                 labs(x = NULL,
+                      y = "Euclidean Distance",
+                      title = paste0("Método = ", .y)) +
+                 scale_x_continuous(breaks = 1:11,
+                                    labels = .x$labels$label) +
+                 scale_y_continuous(expand = FALSE) +
+                 theme_minimal() +
+                 theme(axis.text.x = element_text(angle = 90, hjust = 1,
+                                                  color = "black", size = 25),
+                       axis.text = element_text(color = "black", size = 25),
+                       axis.title = element_text(color = "black", size = 25),
+                       title = element_text(color = "black", size = 25),
+                       panel.grid.major.y = element_line(linewidth = 1,
+                                                         color = "gray80"),
+                       panel.grid.minor.y = element_blank(),
+                       panel.grid.major.x = element_blank(),
+                       panel.grid.minor.x = element_blank()) +
+                 ggview::canvas(height = 10, width = 12)
+
+             ),
+            .progress = TRUE)
